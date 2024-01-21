e7c46b22ef34bb8614303eaee0e75015dbcdea05
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2024-01-22 00:37:53 +0100

    Update:Quiz.py

--- Pages/Quiz.py
+++ Pages/Quiz.py
@@ -1,9 +1,11 @@
 import streamlit as st
-
+from Content import QuizTraining
 def main():
     st.title("Neural Networks Quiz")
-    st.write("In this page we will test your knowledge of neural networks")
+    st.write("In this page we will test your knowledge of neural networks.")
+    st.write("Your Task is to create a model that gets the best accuracy on the following dataset.")
 
+    QuizTraining.draw(st)
 
 if __name__ == "__main__":
     main()
