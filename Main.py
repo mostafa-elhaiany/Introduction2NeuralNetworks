88ee42a5aafe5d77e63c05939d64aa5c37de794a
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2024-01-21 23:14:12 +0100

    Feat: folder structure

---
+++ Main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def main():
+    st.title("Introduction to Neural Networks")
+    st.write("This Streamlit app is designed to be an educational and hands-on tool for individuals interested in Neural Networks. It provides an intuitive interface to experiment with mathematical functions, generate datasets, and observe how neural networks learn from and predict on different data patterns.")
+    st.divider()
+
+    st.divider()
+    
+    st.divider()
+
+    st.divider()
+
+if __name__ == "__main__":
+    main()
---
+++ Pages/Quiz.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("Neural Networks Quiz")
+    st.write("In this page we will test your knowledge of neural networks")
+
+
+if __name__ == "__main__":
+    main()
